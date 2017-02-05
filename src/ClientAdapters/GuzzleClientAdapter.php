5cbe6ce9152c1363437a7ea18cf60dad8b956d6e
James Murray <jaimz@vertigolabs.org>
2017-02-05 10:08:01 -0500

    Fix for issue #2: Registered account, supplied API key, getForecast() returns null

--- src/ClientAdapters/GuzzleClientAdapter.php
+++ src/ClientAdapters/GuzzleClientAdapter.php
@@ -63,16 +63,15 @@ class GuzzleClientAdapter implements ClientAdapterInterface
         }
 
         $response = $this->guzzleClient->get($this->requestedUrl);
+
+        $cacheDirectives = $this->buildCacheDirectives($response);
+
         $this->responseHeaders = [
-            'cache' => [
-                'maxAge' => (int)trim(substr($response->getHeader('cache-control'),
-                        strrpos($response->getHeader('cache-control'), '=') + 1)),
-                'expires' => $response->getHeader('expires')
-            ],
-            'responseTime' => (int)$response->getHeader('x-response-time'),
-            'apiCalls' => (int)$response->getHeader('x-forecast-api-calls')
+            'cache' => $cacheDirectives,
+            'responseTime' => (int)current($response->getHeader('x-response-time')),
+            'apiCalls' => (int)current($response->getHeader('x-forecast-api-calls'))
         ];
-        return $response->json();
+        return json_decode($response->getBody(), true);
     }
 
     /**
@@ -83,5 +82,103 @@ class GuzzleClientAdapter implements ClientAdapterInterface
     public function getHeaders()
     {
         return $this->responseHeaders;
+    }
+
+    /**
+     * Builds the cache directives from response headers.
+     *
+     * @param $response
+     *
+     * @return string[]
+     */
+    protected function buildCacheDirectives($response)
+    {
+        $cacheControlHeader = null;
+        if ($response->hasHeader('cache-control')) {
+            $cacheControlHeader = $this->parse_header($response->getHeader('cache-control'));
+            $cacheControlHeader = current($cacheControlHeader);
+            $cacheControlHeader = (isset($cacheControlHeader['max-age'])?$cacheControlHeader['max-age']:null);
+        }
+
+        $expiresHeader = null;
+        if ($response->hasHeader('expires')){
+            $expiresHeader = join(' ',array_column($this->parse_header($response->getHeader('expires')),0));
+        }
+
+        return array_filter([
+            'maxAge'=>$cacheControlHeader,
+            'expires'=>$expiresHeader,
+        ]);
+    }
+
+    /**
+     * This is taken from the GuzzleHTTP/PSR7 library,
+     * see https://github.com/guzzle/psr7 for more info
+     *
+     * Parse an array of header values containing ";" separated data into an
+     * array of associative arrays representing the header key value pair
+     * data of the header. When a parameter does not contain a value, but just
+     * contains a key, this function will inject a key with a '' string value.
+     *
+     * @param string|array $header Header to parse into components.
+     *
+     * @return array Returns the parsed header values.
+     */
+    protected function parse_header($header)
+    {
+        static $trimmed = "\"'  \n\t\r";
+        $params = $matches = [];
+
+        foreach ($this->normalize_header($header) as $val) {
+            $part = [];
+            foreach (preg_split('/;(?=([^"]*"[^"]*")*[^"]*$)/', $val) as $kvp) {
+                if (preg_match_all('/<[^>]+>|[^=]+/', $kvp, $matches)) {
+                    $m = $matches[0];
+                    if (isset($m[1])) {
+                        $part[trim($m[0], $trimmed)] = trim($m[1], $trimmed);
+                    } else {
+                        $part[] = trim($m[0], $trimmed);
+                    }
+                }
+            }
+            if ($part) {
+                $params[] = $part;
+            }
+        }
+
+        return $params;
+    }
+
+    /**
+     * This is taken from the GuzzleHTTP/PSR7 library,
+     * see https://github.com/guzzle/psr7 for more info
+     *
+     * Converts an array of header values that may contain comma separated
+     * headers into an array of headers with no comma separated values.
+     *
+     * @param string|array $header Header to normalize.
+     *
+     * @return array Returns the normalized header field values.
+     */
+    protected function normalize_header($header)
+    {
+        if (!is_array($header)) {
+            return array_map('trim', explode(',', $header));
+        }
+
+        $result = [];
+        foreach ($header as $value) {
+            foreach ((array) $value as $v) {
+                if (strpos($v, ',') === false) {
+                    $result[] = $v;
+                    continue;
+                }
+                foreach (preg_split('/,(?=([^"]*"[^"]*")*[^"]*$)/', $v) as $vv) {
+                    $result[] = trim($vv);
+                }
+            }
+        }
+
+        return $result;
     }
 }
